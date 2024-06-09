26e2c2db3e4ae5aadd81303f83b22fdc3592995e
thwo93 <cos38zb@outlook.com>
2024-06-09 15:32:12 +0800

    Modify realize.go

--- realize.go
+++ realize.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# A each respond main important authority base.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
