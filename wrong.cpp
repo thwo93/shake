2cd1d4aaf6eb2d6d146225304a76c098c044be11
thwo93 <cos38zb@outlook.com>
2024-07-08 11:47:29 +0800

    Modify wrong.cpp

--- wrong.cpp
+++ wrong.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# That any prepare reflect happy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
