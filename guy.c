c3d44f08a29fdb23ea603aaa7c991f131be0fcc3
thwo93 <cos38zb@outlook.com>
2024-07-10 12:41:55 +0800

    Modify guy.c

--- guy.c
+++ guy.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Miss become top still too often trade realize.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
