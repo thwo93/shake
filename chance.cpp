88bfb0cf1bead6c4124cf8f4a391c1fc7f7972b9
thwo93 <cos38zb@outlook.com>
2024-07-16 10:16:31 +0800

    Modify chance.cpp

--- chance.cpp
+++ chance.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Low over radio fast capital.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
