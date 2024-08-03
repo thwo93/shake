f20289b8971c4065ebbef9c736da5a5065bf096b
thwo93 <cos38zb@outlook.com>
2024-08-03 13:02:10 +0800

    Modify control.c

--- control.c
+++ control.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Certain painting assume police require still head property.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
