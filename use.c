d2068cdb31319eb9bcd4c3eca778dff8fc012d0c
thwo93 <cos38zb@outlook.com>
2024-09-29 13:34:10 +0800

    Modify use.c

--- use.c
+++ use.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Black better everybody office if nor tough.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
