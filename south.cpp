0d38c67eb715a1c6c1545e29e64699ed1d134f2f
thwo93 <cos38zb@outlook.com>
2024-10-09 10:50:43 +0800

    Modify south.cpp

--- south.cpp
+++ south.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Use radio out show.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
