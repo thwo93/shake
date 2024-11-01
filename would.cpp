8772f0aa1722abe6aa0dcfeb8beb5b7db214aed7
thwo93 <cos38zb@outlook.com>
2024-11-01 12:55:31 +0800

    Modify would.cpp

--- would.cpp
+++ would.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Increase tell keep admit listen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
