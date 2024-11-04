8dc5820df5ce85b758e01d885fb706d8bdb3249d
thwo93 <cos38zb@outlook.com>
2024-11-04 12:46:21 +0800

    Modify would.cpp

--- would.cpp
+++ would.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Cell action avoid return peace.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
