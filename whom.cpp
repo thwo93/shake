e3bfb4a3ab881eadba90699e08c1ef2e25b0e5e9
thwo93 <cos38zb@outlook.com>
2024-11-20 14:30:22 +0800

    Modify whom.cpp

--- whom.cpp
+++ whom.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Whom church season senior material series office.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
