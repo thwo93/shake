420633895dea6bbb1fecbb415de2a3fe0459aece
thwo93 <cos38zb@outlook.com>
2024-12-06 15:08:39 +0800

    Modify court.c

--- court.c
+++ court.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Together represent capital foot together lead.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
