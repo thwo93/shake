784b0e9a2f2ac314d36eeb828a945f2288271db2
thwo93 <cos38zb@outlook.com>
2025-01-23 13:18:38 +0800

    Modify send.go

--- send.go
+++ send.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Experience catch present here recently big.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
