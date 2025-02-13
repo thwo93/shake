9dec872b2864ecbb53a4ba40f15ddf278fb51e8e
thwo93 <cos38zb@outlook.com>
2025-02-13 14:55:27 +0800

    Modify remain.cpp

--- remain.cpp
+++ remain.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Race second purpose when government.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
