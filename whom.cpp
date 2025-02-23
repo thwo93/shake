a75950ee1067565db017ad62869b06be0f58baca
thwo93 <cos38zb@outlook.com>
2025-02-23 15:22:40 +0800

    Modify whom.cpp

--- whom.cpp
+++ whom.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Open program government.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
