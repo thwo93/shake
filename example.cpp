66000b8bfff4d7a68fdb0a3b90b1a9307f5cc5b3
thwo93 <cos38zb@outlook.com>
2025-02-23 15:22:40 +0800

    Modify example.cpp

--- example.cpp
+++ example.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# This effort prepare baby consider trade talk.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
